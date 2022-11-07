1613a2466a546e87d09673e0a4b441fbca2177e0
welton-zeyerq <106247800+welton-zeyerq@users.noreply.github.com>
2022-11-07 04:27:14 +0000

    Update dns_resolver.py
    fix errors

--- dns_resolver.py
+++ dns_resolver.py
@@ -16,53 +16,42 @@ if len(sys.argv) <=1:
     helplk()
     sys.exit()
 
-elif len(sys.argv) >=4:
+if len(sys.argv) >=4:
     helplk()
     sys.exit()
 
 def resolvelk():
     url = str(sys.argv[2])
-    try:
-        gt = (socket.gethostbyname(url))
-        print(gt)
-    except KeyboardInterrupt:
-        sys.exit()
-    else:
-        sys.exit()
-    finally:
-        sys.exit()
+    gt = (socket.gethostbyname(url))
+    print(gt)
 
 def reverselk():
     arg = str(sys.argv[2])
+    name, alias, addresslist = socket.gethostbyaddr(arg)
+    gt2 = (name, alias, addresslist)
+    print(gt2)
+
+if __name__ == "__main__":
     try:
-        name, alias, addresslist = socket.gethostbyaddr(arg)
-        gt2 = (name, alias, addresslist)
-        print(gt2)
+        choice = str(sys.argv[1])
+        if choice == "-h":
+            helplk()
+        elif choice == "--help":
+            helplk()
+        elif choice == "-r":
+            if len(sys.argv) !=2:
+                resolvelk()
+            else:
+                helplk()
+        elif choice == "-x":
+            if len(sys.argv) !=2:
+                reverselk()
+            else:
+                helplk()
+        else:
+            print("ERROR, NO OPTION OR DIGITATION ERROR")
     except KeyboardInterrupt:
         sys.exit()
-    else:
+    except Exception as error:
+        print(error)
         sys.exit()
-    finally:
-        sys.exit()
-
-choice = str(sys.argv[1])
-if choice == "-r":
-    if len(sys.argv) !=2:
-        resolvelk()
-    else:
-        helplk()
-
-elif choice == "-x":
-    if len(sys.argv) !=2:
-        reverselk()
-    else:
-        helplk()
-
-elif choice == "-h":
-    helplk()
-
-elif choice == "--help":
-    helplk()
-
-else:
-    print("ERROR, NO OPTION OR DIGITATION ERROR")
